617ff24e20e9e473c57771c057e3b1cb1d4859f0
Robert WC Seifert <rseifert.phone@gmail.com>
2018-10-27 07:37:31 -0400

    Added missing DataPool class

--- src/main/java/com/builtbroken/visualization/data/GridPoint.java
+++ src/main/java/com/builtbroken/visualization/data/GridPoint.java
@@ -1,7 +1,5 @@
 package com.builtbroken.visualization.data;
 
-import com.builtbroken.atomic.map.data.DataPool;
-
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 10/26/2018.
